e3eb9ec18b3aff8fcf0af27e692a66e623f2ca05
sveta-confeta <_svet_@tut.by>
2022-02-11 20:11:43 +0300

    checkbox stories

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -3,33 +3,62 @@ import {OnOff} from "../components/OnOff/OnOff";
 
 
 export default {
-  title: 'input',
-  component: OnOff,
+    title: 'input',
+    component: OnOff,
 };
 
 export let UncontrollInput = () => <input/>
 
 export let TrackingValueInput = () => {
-  const[value,setValue]=useState('');
-  const onChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
-    setValue(e.currentTarget.value);
-  }
-  return <>
-    <input value={value} onChange={onChangeHandler}/> -{value}
+    const [value, setValue] = useState('');
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    }
+    return <>
+        <input value={value} onChange={onChangeHandler}/> -{value}
     </>
 }
 
 //пример с рефами:
 export let GetTrackingValue = () => {
-  const[value,setValue]=useState('');
-  const inputRef=useRef<HTMLInputElement>(null);
-  const onClickHandler=()=>{const el=inputRef.current as HTMLInputElement
-    setValue(el.value)};
+    const [value, setValue] = useState('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const onClickHandler = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    };
 
-  return <>
-    <input ref={inputRef}/> <button onClick={onClickHandler}>save</button> - actual value {value}
-  </>
+    return <>
+        <input ref={inputRef}/>
+        <button onClick={onClickHandler}>save</button>
+        - actual value {value}
+    </>
 }
+//неконтролируемый инпут
+export let UnControlledInputGg = () => <input value={'tra-ta-ta'}/>
 
-export let ControlledInput = () => <input value = {'tra-ta-ta'} />
+
+//чекбокс
+export const ControlledCheckboxg = () => {
+    const [value, setValue] = useState(true);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.checked);
+    }
+    return <input type="checkbox" checked={value} onChange={onChangeHandler}/>
+}
+//селект
+export const ControlledSelect = () => {
+     const[parentvalue,setParentValue]=useState<string|undefined> ('2')
+     const onChange=(e:ChangeEvent<HTMLSelectElement>)=>{
+         setParentValue(e.currentTarget.value);
+     }
+    return <>
+        <select value={parentvalue} onChange={onChange}>
+            <option>none</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Vitebsk</option>
+          <option value={'3'}>Kiev</option>
+        </select>
+    </>
+}
 
