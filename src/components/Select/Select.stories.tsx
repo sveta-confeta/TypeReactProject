41961488f47b44fb5fd7a982292f40d528441ae4
sveta-confeta <_svet_@tut.by>
2022-02-12 20:47:15 +0300

    select stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Select from "./Select";
 import {action} from "@storybook/addon-actions";
-import {UncontrolledAccordeon} from "../Accordeon Uncontroller/AccordeonUncontroller";
+
 
 
 export default {
     title: 'Select stories',
     component: Select,
 };
-//
-export const TTTAAA = () => {
 
-    return <Select value={'1'} onChange={action("value change")} items={[
-        {value:1,title:'Minsk'},
-        {value:2,title:'Kiev'},
-        {value:3,title:'Moskow'},
+export const SelectMode = () => {
+    const [value, setValue] = useState('2');
+    return <>
+        <Select value={value} onChange={setValue} items={[
+            {value: '1', title: 'Minsk'},
+            {value: '2', title: 'Kiev'},
+            {value: '3', title: 'Moskow'},
+        ]}/>
+    </>
+}
+
+export const NotValue = () =>{
+const [value, setValue] = useState(null);
+return<>
+    <Select value={value} onChange={setValue} items={[
+        {value:'1',title:'Minsk'},
+        {value:'2',title:'Kiev'},
+        {value:'3',title:'Moskow'},
     ]}/>
-};
+</>
+    }
+;
 
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, {useState, KeyboardEvent} from 'react';
+import s from './Select.module.css'
 
 type ItemType = {
     title: string
     value: any
+
 }
 
 
 type SelectPropsType = {
-    value: any//селект принимает текущее выбранное значение
+    value?: any//селект принимает текущее выбранное значение
     onChange: (value: any) => void//мы хотим знать когда селект изменится
     items: Array<ItemType>//как в акардеоне-дайте нам items
 
 }
 
-export function Select (props: SelectPropsType){
-    // const[parentvalue,setParentValue]=useState<string|undefined> ('2')
-    // const onChange=(e:ChangeEvent<HTMLSelectElement>)=>{
-    //     setParentValue(e.currentTarget.value);
-    // }
+//самодельный селект
+export function Select(props: SelectPropsType) {
+    const [active, setActive] = useState(false);
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+
+    const selectedItem = props.items.find(i => i.value === props.value);
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue);
+
+    const toggleItems = () => setActive(!active);
+    const itemClick = (value: string) => {
+        props.onChange(value)
+        toggleItems() //при выборе селект закрывается
+    }
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+       for(let i=0;i < props.items.length; i++){
+           if(props.items[i].value===hoveredElementValue){
+               if(props.items[i+1]){
+                   props.onChange(props.items[i+1].value);
+                   break;
+               }
+
+           }
+       }
+    }
+
+
     return (
-        <div>
-            <div>{}</div>
-            {props.items.map(i=><div>{i.title}</div>)}
-        </div>
-        // <select value={parentvalue} onChange={onChange}>
-        //     <option>none</option>
-        //     <option value={'1'}>Minsk</option>
-        //     <option value={'2'}>Vitebsk</option>
-        // </select>
+        <>
+
+            <div className={`${s.wrapper} `} onKeyUp={onKeyUp} tabIndex={0}>
+                <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+                {active &&
+                    <div className={s.items}>
+                        {props.items.map(i => <div key={i.value} onMouseEnter={() => {
+                            setHoveredElementValue(i.value)
+                        }} className={s.item + ' ' + (hoveredItem === i ? s.selected : '')}
+                                                   onClick={() => itemClick(i.value)}>{i.title}</div>)}
+                        {/*условие внутри -это подсветка внутри селекта*/}
+                    </div>
+                }
+            </div>
+        </>
     );
 };
 
